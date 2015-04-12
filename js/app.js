a022773ca478ad6c13dbc096ef54c248e0abd3b0
Lia Abbott <julia@teamcmp.com>
2015-04-11 20:59:46 -0400

    adding default hash

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 $(function() {
+	var pages = ['dashboard', 'overview', 'key-features', 'industry-applications', 'technical-info', 'faq'];
+
 	showPage(window.location.hash);
 
 	$(window).on('hashchange', function(){
@@ -6,6 +8,12 @@ $(function() {
 	});
 
 	function showPage(newPage) {
+		console.log($.inArray(newPage, pages));
+
+		if ($.inArray(newPage, pages) == -1) {
+			window.location.hash = '#dashboard';
+		}
+
 		$('.page').removeClass('visible').addClass('hidden');
 		$(newPage).removeClass('hidden').addClass('visible');
 
